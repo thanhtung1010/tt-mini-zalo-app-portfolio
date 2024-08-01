1375591b58f2db66bbe963da0c4a60c6baed1cb3
tung.trinh <trinhthanhtung1010@gmail.com>
2024-08-01 14:49:06 +0700

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -24,19 +21,14 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-      postcss(),
     ],
-    external: ["react", "react-dom"],
   },
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
+    input: "dist/esm/index.js",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts.default()],
-    external: [/\.css$/],
   },
 ];
